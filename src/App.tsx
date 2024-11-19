1635e0739f97610bb9976592b2b99012d04bd812
Meghraj <hello.meghrajjj@gmail.com>
2024-11-19 18:58:59 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-500">Welcome to Social Media App</h1>
+      <p className="text-lg text-gray-600 mt-4">Tailwind CSS is working!</p>
     </div>
   );
-}
+};
 
 export default App;
